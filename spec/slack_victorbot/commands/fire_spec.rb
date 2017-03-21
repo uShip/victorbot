9933eecf7af10be5be2e9fa9cb1633997633059b
Evan Machnic <emachnic@uship.com>
2017-03-21 11:28:14 -0500

    Filter out sensitive data from VCR cassette

--- spec/slack_victorbot/commands/fire_spec.rb
+++ spec/slack_victorbot/commands/fire_spec.rb
@@ -1,4 +1,5 @@
 describe SlackVictorbot::Commands::Fire do
+  vcr_options = { cassette_name: 'victorops' }
 
   def app
     SlackVictorbot::Bot.instance
@@ -6,12 +7,10 @@ describe SlackVictorbot::Commands::Fire do
 
   subject { app }
 
-  context 'successfully creates a new incident' do
-    use_vcr_cassette :victorops
-
+  context 'successfully creates a new incident', vcr: vcr_options do
     it 'returns the Victorops incident number if successful' do
       expect(message: "#{SlackRubyBot.config.user} fire It works!", channel: 'channel')
-        .to respond_with_slack_message(/created Victorops incident 2296/i)
+        .to respond_with_slack_message(/created Victorops incident/i)
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,10 +10,10 @@ VCR.configure do |config|
   config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
   config.hook_into :webmock
 
-  config.before_record do |i|
-    i.request.headers.delete('X-VO-Api-Id')
-    i.request.headers.delete('X-VO-Api-Key')
-  end
+  config.filter_sensitive_data('<API_ID>') { ENV['VICTOROPS_API_ID'] }
+  config.filter_sensitive_data('<API_KEY>') { ENV['VICTOROPS_API_KEY'] }
+  config.filter_sensitive_data('<VICTOROPS_USER>') { ENV['VICTOROPS_USER'] }
+  config.filter_sensitive_data('<VICTOROPS_TEAM>') { ENV['VICTOROPS_TEAM'] }
 end
 
 RSpec.configure do |config|
